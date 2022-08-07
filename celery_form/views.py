739057b5e668889825bf6cf47b0a66c84419a502
Danylo Tseselskyi <danylotseselskyi@gmail.com>
2022-08-07 21:26:03 +0300

    Was added send_email form

--- celery_form/views.py
+++ celery_form/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import PostForm
+from .tasks import send_task
+
+
+def note_form(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            email_recipient = form.cleaned_data['email_recipient']
+            message = form.cleaned_data['message']
+            deadline = form.cleaned_data['date_and_time']
+            send_task.apply_async((subject, email_recipient, message), eta=deadline)
+            return redirect('celery_form:index')
+    else:
+        form = PostForm()
+    return render(
+        request,
+        'celery_form/index.html',
+        {
+            'note_form': form}
+        )
